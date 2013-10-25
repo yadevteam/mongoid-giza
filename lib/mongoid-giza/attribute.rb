5c01d9f734ae3444f74a23974c65ac4a62672e16
Maurício Batista <eddloschi@gmail.com>
2013-10-25 16:06:21 -0200

    Created documentation

--- lib/mongoid-giza/attribute.rb
+++ lib/mongoid-giza/attribute.rb
@@ -1,7 +1,10 @@
 module Mongoid
   module Giza
+    # Represents an Sphinx index attribute
     class Attribute
       class << self;
+
+        # Defines the array of currently supported Sphix attribute types
         def types
           [:uint, :bool, :bigint, :timestamp, :str2ordinal,
             :float, :multi, :string, :json, :str2wordcount]
@@ -10,6 +13,15 @@ module Mongoid
 
       attr_accessor :name, :type, :block
 
+      # Creates a new attribute with name, type and an optional block
+      #
+      # If a block is given then it will be evaluated for each instance of the class being indexed
+      # and the resulting value will be the attribute value.
+      # Otherwise the attribute value will be the value of the corresponding object field
+      #
+      # Parameters::
+      #   * [ +Symbol+ +name+ ] the name of the attribute
+      #   * [ +Symbol+ +type+ ] the type of the attribute. Must be one of the types defined in +Mongoid+::+Giza+::+Attribute+::+types+
       def initialize(name, type, &block)
         raise TypeError,
           "attribute type not supported. " \
--- lib/mongoid-giza/index.rb
+++ lib/mongoid-giza/index.rb
@@ -1,7 +1,11 @@
 module Mongoid
   module Giza
+    # Represents a Sphinx index
     class Index
       class << self;
+
+        # Returns a hash in which each key is a class accepted by +Mongoid+
+        # and its value is a compatible Sphix attribute type
         def types_map
           {
             Regexp => :string,
@@ -25,6 +29,13 @@ module Mongoid
 
       attr_accessor :klass, :settings, :fields, :attributes
 
+      # Creates a new index with a class, which should include Mongoid::Document, and an optional settings hash.
+      #
+      # Note that no validations are made on class, so classes that behave like Mongoid::Document should be fine.
+      #
+      # Parameters::
+      #   * [ Class klass ] the class whose objects will be indexed
+      #   * [ Hash settings ] an optional settings hash to be forwarded to Riddle
       def initialize(klass, settings={})
         @klass = klass
         @settings = settings
@@ -32,10 +43,32 @@ module Mongoid
         @attributes = []
       end
 
+      # Adds a fulltext search field to the index with the corresponding name.
+      #
+      # If a block is given then it will be evaluated for each instance of the class being indexed
+      # and the resulting string will be the field value.
+      # Otherwise the field value will be the value of the corresponding object field
+      #
+      # Parameters::
+      #   * [ +Symbol+ +name+ ] the name of the field
+      #   * [ +Proc+ +block+ ] an optional block to be evaluated
       def field(name, &block)
         @fields << Mongoid::Giza::Field.new(name, block)
       end
 
+      # Adds an attribute to the index with the corresponding name.
+      #
+      # If a type is not given then it will try to fetch the type of the corresponding class field,
+      # falling back to +:string+
+      #
+      # If a block is given then it will be evaluated for each instance of the class being indexed
+      # and the resulting value will be the attribute value.
+      # Otherwise the attribute value will be the value of the corresponding object field
+      #
+      # Parameters::
+      #   * [ +Symbol+ +name+ ] the name of the attribute
+      #   * [ +Symbol+ +type+ ] an optional attribute type
+      #   * [ +Proc+ +block+ ] an optional block to be evaluated
       def attribute(name, type=nil, &block)
         if type.nil?
           field = @klass.fields[name.to_s]
