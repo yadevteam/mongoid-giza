042e3f664a4022b9486e4df8a1016a787d3796f3
Maurício Batista <eddloschi@gmail.com>
2013-10-24 23:22:45 -0200

    Added Mongoid matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require "mongoid-rspec"
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
@@ -14,4 +16,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+
+  config.include Mongoid::Matchers, type: :model
 end
