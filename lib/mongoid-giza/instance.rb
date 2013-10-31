59e804a8af06d7c69303da4b4a29bddc14e65c8a
Maurício Batista <eddloschi@gmail.com>
2013-10-30 22:53:11 -0200

    Instance documentation

--- lib/mongoid-giza/instance.rb
+++ lib/mongoid-giza/instance.rb
@@ -1,7 +1,11 @@
 module Mongoid
   module Giza
+    ##
+    # Holds the defined indexes
     module Instance
       class << self
+        ##
+        # Returns the collection of indexes defined so far
         def indexes
           @indexes ||= {}
         end
