f736ed570cb8801de2740455ba456cf182be4d84
Maurício Batista <eddloschi@gmail.com>
2013-11-01 17:22:21 -0200

    Further improve specs

--- spec/mongoid-giza_spec.rb
+++ spec/mongoid-giza_spec.rb
@@ -1,18 +1,13 @@
 require "spec_helper"
 
 describe Mongoid::Giza do
-  let(:person_class) do
+  before :all do
     class Person
       include Mongoid::Document
       include Mongoid::Giza
 
       field :name, type: String
       field :age, type: Integer
-
-      search_index do
-        field :name
-        attribute :age
-      end
     end
   end
 
@@ -22,39 +17,31 @@ describe Mongoid::Giza do
     index
   end
 
-  let(:index_class) { allow(Mongoid::Giza::Index).to receive(:new).with(kind_of(Class)) { index } }
+  let(:new_index) { allow(Mongoid::Giza::Index).to receive(:new).with(Person) { index } }
+
+  let(:config_indexes) { allow(Mongoid::Giza::Instance).to receive(:indexes) { double("indexes").as_null_object } }
 
   describe "search_index" do
-    context "with a simple field and a simple attribute" do
+    context "static index" do
       it "should create an index" do
-        allow(index).to receive(:field).with(:name)
-        allow(index).to receive(:attribute).with(:age)
-        expect(Mongoid::Giza::Index).to receive(:new) { index }
-        person_class
+        config_indexes
+        expect(Mongoid::Giza::Index).to receive(:new).with(Person) { index }
+        Person.search_index { }
       end
 
-      it "should create an index field" do
-        allow(index).to receive(:attribute).with(:age)
+      it "should call index methods" do
+        config_indexes
         expect(index).to receive(:field).with(:name)
-        index_class
-        person_class
-      end
-
-      it "should create an index attribute" do
-        allow(index).to receive(:field).with(:name)
-        expect(index).to receive(:attribute).with(:age)
-        index_class
-        person_class
+        new_index
+        Person.search_index { field :name }
       end
 
       it "should register the index" do
-        allow(index).to receive(:field).with(:name)
-        allow(index).to receive(:attribute).with(:age)
         indexes = double("indexes")
         expect(indexes).to receive(:[]=).with(:Person, index)
         expect(Mongoid::Giza::Instance).to receive(:indexes) { indexes }
-        index_class
-        person_class
+        new_index
+        Person.search_index { }
       end
     end
   end
