e7ea522fb773798e3bc1479fa4c11de97d49ab51
Maurício Batista <eddloschi@gmail.com>
2013-11-07 12:43:36 -0200

    Better spec code

--- spec/mongoid-giza/instance_spec.rb
+++ spec/mongoid-giza/instance_spec.rb
@@ -1,10 +1,12 @@
 require "spec_helper"
 
 describe Mongoid::Giza::Instance do
+  let(:instance) { Mongoid::Giza::Instance }
+
   describe "indexes" do
     it "should store new indexes" do
-      Mongoid::Giza::Instance.indexes[:index] = true
-      expect(Mongoid::Giza::Instance.indexes.has_key?(:index)).to eql(true)
+      instance.indexes[:index] = true
+      expect(instance.indexes.has_key?(:index)).to eql(true)
     end
   end
 end
