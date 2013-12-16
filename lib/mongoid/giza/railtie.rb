5cc06027481aac17784c74e7b7d6cf3d289746b2
Maurício Batista <eddloschi@gmail.com>
2013-12-16 19:17:45 -0200

    Defined default xmlpipe_command on rails

--- lib/mongoid/giza/railtie.rb
+++ lib/mongoid/giza/railtie.rb
@@ -3,9 +3,14 @@ require "rails"
 module Mongoid
   module Giza
     class Railtie < Rails::Railtie
+      configuration = Mongoid::Giza::Configuration.instance
+
       initializer "mongoid-giza.load-configuration" do
+        # Sets the default xmlpipe_command
+        configuration.source.xmlpipe_command = "rails r '<%= index.klass %>.sphinx_indexes[:<%= index.name %>].generate_xmlpipe2(STDOUT)'"
+        # Loads the configuration file
         file = Rails.root.join("config", "giza.yml")
-        Mongoid::Giza::Configuration.instance.load(file, Rails.env) if file.file?
+        configuration.load(file, Rails.env) if file.file?
       end
     end
   end
