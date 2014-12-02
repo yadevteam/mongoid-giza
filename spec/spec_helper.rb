272c938bc0c883cc6b6034365f03968542e7e182
Maurício Batista <eddloschi@gmail.com>
2014-12-02 00:34:27 -0200

    Removed deprecated configuration option

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,6 @@ Mongoid.configure do |config|
 end
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
